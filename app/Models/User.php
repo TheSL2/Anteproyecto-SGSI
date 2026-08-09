ad318fa59de282d7b3518992bee68721aa8a69fd
TheSL2 <omarsanluis50@gmail.com>
2026-08-08 18:49:12 -0600

    Feature | Models

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,48 +2,73 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'rol',
+        'area_id',
+        'activo',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
+        'two_factor_secret',
+        'two_factor_recovery_codes',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'activo' => 'boolean',
+            'two_factor_confirmed_at' => 'datetime',
         ];
+    }
+
+    public function tieneConflictoDeInteres(int $areaEvaluadaId): bool
+    {
+        return $this->area_id !== null && $this->area_id === $areaEvaluadaId;
+    }
+
+    public function tieneDosFactorActivo(): bool
+    {
+        return ! empty($this->two_factor_secret) && ! is_null($this->two_factor_confirmed_at);
+    }
+
+    public function area()
+    {
+        return $this->belongsTo(Area::class);
+    }
+
+    public function auditoriasLideradas()
+    {
+        return $this->hasMany(Auditoria::class, 'auditor_lider_id');
+    }
+
+    public function auditoriasComoEquipo()
+    {
+        return $this->belongsToMany(Auditoria::class, 'auditoria_users');
+    }
+
+    public function accionesCorrectivasAsignadas()
+    {
+        return $this->hasMany(AccionCorrectiva::class, 'responsable_id');
+    }
+
+    public function evidenciasSubidas()
+    {
+        return $this->hasMany(Evidencia::class, 'subido_por');
     }
 }
